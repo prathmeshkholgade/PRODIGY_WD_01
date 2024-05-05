d4572b278b36f76055f0f2e2efc23988c7e46e09
prathmesh <prathmeshkholgade1990@gmail.com>
2024-05-05 10:38:32 +0530

    added

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const navbar = document.querySelector(".nav-bar");
-const logoText=document.querySelector("#logo-text")
+const logoText = document.querySelector("#logo-text");
+const navLinks = document.querySelectorAll(".nav-link");
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log(window.scrollY);
   function scrolled() {
     if (window.scrollY > 0) {
       navbar.classList.add("navbar-scrolled");
-      logoText.classList.add("scrolled-logo")
+      logoText.classList.add("scrolled-logo");
     } else {
       navbar.classList.remove("navbar-scrolled");
-      logoText.classList.remove("scrolled-logo")
+      logoText.classList.remove("scrolled-logo");
     }
   }
   window.addEventListener("scroll", scrolled);
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log("nav clicked");
+
+      const targetId = this.getAttribute("href").substring(1);
+
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: "smooth" });
+        effect;
+      }
+    });
+  });
 });
-
-
